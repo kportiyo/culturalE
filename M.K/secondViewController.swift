e2117e884842c0790039e29cf50d18f3e440c4d0
kportillo <student@bluc02mg1hlfh51.local>
2016-04-06 11:24:44 -0500

    initial commit

---
+++ M.K/secondViewController.swift
@@ -0,0 +1,38 @@
+//
+//  secondViewController.swift
+//  M.K
+//
+//  Created by kportillo on 4/6/16.
+//  Copyright © 2016 kportillo. All rights reserved.
+//
+
+import UIKit
+
+class secondViewController: UIViewController
+{
+    
+    @IBOutlet weak var label1: UILabel!
+    
+    override func viewDidLoad()
+    {
+        
+        super.viewDidLoad()
+    
+    }
+    
+    @IBAction func helloButton(sender: UIButton)
+    {
+        UIView.animateWithDuration(1.0, delay: 0.0, options: UIViewAnimationOptions.CurveEaseIn, animations: {
+            self.label1.alpha = 2.0
+            }, completion: nil)
+        
+        UIView.animateWithDuration(1.0, delay: 0.0, options: UIViewAnimationOptions.CurveEaseOut, animations: {
+            self.label1.alpha = 0.0
+            }, completion: nil)
+
+        }
+   
+    
+    
+}
+
